a212073c46f088771b0ef6beaf5dbab497897b26
duan spot <brandennjonge@gmail.com>
2023-09-04 19:24:13 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name ,age ,sex,color,temperament,alive) VALUES (Mr. Chocolate,20,"M',"brown","cool",1);
+INSERT INTO bears (name ,age ,sex,color,temperament,alive) VALUES (Rowdy,10,"M","brown","cool",1);
+INSERT INTO bears (name ,age ,sex,color,temperament,alive) VALUES (Tabitha,20,"F","brown","warm",1);
+INSERT INTO bears (name ,age ,sex,color,temperament,alive) VALUES (Sergeant Brown,40,"M","brown","cool",1);
+INSERT INTO bears (name ,age ,sex,color,temperament,alive) VALUES (Melissa,50,"F","brown","cool",1);
+INSERT INTO bears (name ,age ,sex,color,temperament,alive) VALUES (Grinch,20,"M","brown","cool",1);
+INSERT INTO bears (name ,age ,sex,color,temperament,alive) VALUES (Wendy,20,"F","brown","cool",1);
+INSERT INTO bears (name ,age ,sex,color,temperament,alive) VALUES (NULL,90,"M","brown","cool",1);
